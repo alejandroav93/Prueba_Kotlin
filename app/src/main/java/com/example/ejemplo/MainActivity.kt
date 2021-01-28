f8f25af972a835e6d8c09e2d99f4e72061ebe2ef
Alejandro Alvarez V <59927580+alejandroav93@users.noreply.github.com>
2021-01-28 08:01:49 -0600

    Prueba 28 Enero

--- app/src/main/java/com/example/ejemplo/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo/MainActivity.kt
@@ -1,3 +1,9 @@
+/*
+* Universidad del Valle de Guatemala
+* Alejandro ALvarez
+* Carné 12429
+* Guatemala 28 Enero de 2021
+ */
 package com.example.ejemplo
 
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +14,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         variablesYConstantes()
+        arrays()
+        sentenciaIf()
+        sentenciaWhen()
+        maps()
     }
 
     //Esta es una prueba de comentario
@@ -16,7 +26,7 @@ class MainActivity : AppCompatActivity() {
      */
 
     //Variables y Constantes
-    private fun variablesYConstantes(){
+    private fun variablesYConstantes() {
         //Variables
         var myFirstVariable = "Esta es una prueba"
         var myFirstNumbrer = 10000
@@ -26,56 +36,134 @@ class MainActivity : AppCompatActivity() {
         println(mySecondVariable)
 
         //Constantes
-            val myFirstConstante = "Seguimos con la prueba"
-            println(myFirstConstante)
+        val myFirstConstante = "Seguimos con la prueba"
+        println(myFirstConstante)
 
         //Tipos de datos
-            val myString3 = myFirstVariable + " " + mySecondVariable
-            println(myString3)
+        val myString3 = myFirstVariable + " " + mySecondVariable
+        println(myString3)
 
         //Enteros  = Byte -127 +128 short -32768 +32767 Int Long
-            val myInt = 100
-            val myInt2 = 200
-            val myInt3 = myInt + myInt2
-            println(myInt3)
+        val myInt = 100
+        val myInt2 = 200
+        val myInt3 = myInt + myInt2
+        println(myInt3)
 
         //Decimales Float 32bit Double 64bit
-            val myFloat = 100.5
-            val myFloat2 = 199.5
-            val myInt10 = 10
-            val myFloat3 = myFloat + myFloat2 + myInt10
-            println(myFloat3)
+        val myFloat = 100.5
+        val myFloat2 = 199.5
+        val myInt10 = 10
+        val myFloat3 = myFloat + myFloat2 + myInt10
+        println(myFloat3)
 
         //Boolean
         val myBool = true
         val myBool1 = false
         println(myBool)
     }
-    private fun sentenciaIf(){
-        val myNumber = 7
-        if(myNumber <= 10 && myNumber >5){
-            println("$myNumber es  menor o igual que 10")
+
+    fun arrays() {
+        val name = "Alejandro"
+        val apellido = "Alvarez"
+        val universidad = "Del Valle de Guatemala"
+        val ciudad = "Guatemala"
+
+        //crear array
+        val myArray: ArrayList<String> = arrayListOf<String>()
+
+        //agregar datos
+        myArray.add(name)
+        myArray.add(apellido)
+        myArray.add(universidad)
+        myArray.add(ciudad)
+
+        println(myArray)
+
+        //agregar un conjunto de datos
+        myArray.addAll(listOf("Prueba UVG", "Uso Array Kotlin"))
+        println(myArray)
+
+        //acceso a datos
+        val myCompany: String = myArray[2]
+        println(myArray)
+
+        //modificar datos
+        myArray[2] = "Solo del Valle"
+        println(myArray)
+
+        //eliminar Datos
+        myArray.removeAt(2)
+        println(myArray)
+
+        //recorrer el array
+        myArray.forEach {
+            String
+            println(it)
         }
-        else{
-            println("$myNumber es mayor que 10")
+    }
+        private fun sentenciaIf() {
+            val myNumber = 7
+            if (myNumber in 6..10) {
+                println("$myNumber es  menor o igual que 10")
+            } else {
+                println("$myNumber es mayor que 10")
+            }
+
         }
 
-    }
-    private fun sentenciaWhen(){
-        val country = "USA"
-        when (country){
-            "Guatemala" -> {
-                println("El idioma es español")
+        private fun sentenciaWhen() {
+            val country = "USA"
+            when (country) {
+                "Guatemala" -> {
+                    println("El idioma es español")
+                }
+                "Nicaragua" -> {
+                    println("El idioma es español")
+                }
+                "USA" -> {
+                    println("El idioma es Ingles")
+                }
+                else -> {
+                    println("No conocemos el idioma")
+                }
             }
-            "Nicaragua" -> {
-                println("El idioma es español")
-            }
-            "USA" -> {
-                println("El idioma es Ingles")
-            }
-            else -> {
-                println("No conocemos el idioma")
+            //when con in
+
+            val codigo = 10
+
+            when (codigo) {
+                1, 2, 3, 4, 5 -> {
+                    println("Debe usar camisa azul")
+                }
+                in 6..15 -> {
+                    println("Debe usar camisa roja")
+                }
             }
         }
-    }
+
+        fun maps() {
+        var myMap : Map<String, Int> = mapOf()
+            println(myMap)
+
+            //Agregar datos
+            myMap = mutableMapOf("Uno" to 1, "Dos" to 2, "Tres" to 3, "Cuatro" to 4)
+            println(myMap)
+
+            //Agregar un dato
+            myMap["Cinco"] = 5
+            myMap["Seis"] = 6
+            println(myMap)
+
+            //Agregar con put
+            myMap.put("Siete", 7)
+            println(myMap)
+
+            //Accesar a los datos
+            println(myMap["Seis"])
+
+            //Eliminar Datos
+            myMap.remove("Seis")
+            println(myMap)
+        }
+
 }
